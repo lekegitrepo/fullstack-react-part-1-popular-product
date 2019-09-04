968108b09ce2dccb418f63f1181ba8618e8f24a5
leketemi <leketemi@gmail.com>
2019-09-04 11:40:10 +0100

    sort each item by number of votes

--- public/js/app-2.js
+++ public/js/app-2.js
@@ -1,6 +1,9 @@
 class ProductList extends React.Component {
   render() {
-    const productComponents = Seed.products.map((product) => (
+    const products = Seed.products.sort((a, b) => (
+      b.votes - a.votes
+    ))
+    const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
         id={product.id}
@@ -12,11 +15,11 @@ class ProductList extends React.Component {
         productImageUrl={product.productImageUrl}
        />
     ));
-    // return (
-    //   <div className='ui unstackable items'>
-    //
-    //   </div>
-    // );
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
+      </div>
+    );
   }
 }
 
